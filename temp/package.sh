0b41588635a6e8c83ebfefb2e812990d69886ea5
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-07-31 11:46:34 +0800

    fix script

--- temp/package.sh
+++ temp/package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-cp ../files/$1 ../temp
+cp --preserve=timestamps ../files/$1 ../temp/BallanceMMOClient.bmod
 cd ../temp
-zip BallanceMMO_$2.zip $1 GameNetworkingSockets.dll libcrypto-1_1.dll libprotobuf.dll
-rm $1
+zip BallanceMMO_$2.zip BallanceMMOClient.bmod GameNetworkingSockets.dll libcrypto-1_1.dll libprotobuf.dll
+rm BallanceMMOClient.bmod
 mv BallanceMMO_$2.zip ../packages/
